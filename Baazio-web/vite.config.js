1c37795d63fa05ebd1409d0ebd4ac30e768541ac
AjalaJoseph <ajalaoluwafikayomi27@gmail.com>
2026-08-26 11:36:13 +0100

    Update vite.config.js

--- Baazio-web/vite.config.js
+++ Baazio-web/vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // Forces Vite to bypass Rolldown's native engine minifier and use rock-solid esbuild instead
+    minify: 'esbuild',
+    // Generates source maps so Vercel can display hidden errors if any syntax breaks
+    sourcemap: true
+  }
 })
